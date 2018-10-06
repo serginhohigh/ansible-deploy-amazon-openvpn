db1973965d297d46bbb123db2896b5b27e2a4d55
root <root@debian9-server.project.internal>
2018-10-07 00:31:13 +0300

    Some changes in the project

---
+++ roles/configure-proxy-instances/templates/openvpn-proxy01.backup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+state=$3
+ip='10.50.1.4'
+mac=`ip link show eth0 | awk '/ether/ {print $2}'`
+iface_id=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/"$mac"/interface-id`
+
+case "$state" in
+        "MASTER")
+                ec2-assign-private-ip-addresses \
+                        --region eu-central-1 \
+                        --aws-access-key 'AKIAIEBB23XJTJ3SDZOA' \
+                        --aws-secret-key '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' \
+                        --network-interface "$iface_id" \
+                        --secondary-private-ip-address "$ip" \
+                        --allow-reassignment && ip addr add dev eth0 "$ip" label eth0:2
+                        ;;
+        "BACKUP")
+                        ip addr del dev eth0 "$ip"/32
+                        ;;
+        "FAULT")
+                        ip addr del dev eth0 "$ip"/32
+                        ;;
+        *)
+                        ;;
+esac
